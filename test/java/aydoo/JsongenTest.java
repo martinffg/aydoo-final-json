b04a680b3c2c789f3ce54bf6a8260f05812e9304
ignaciolunaechechuri <lunaechechri.ignacio@gmail.com>
2015-12-06 01:08:30 -0300

    Se agregaron nuevos test a la clase jsongen

--- test/java/aydoo/JsongenTest.java
+++ test/java/aydoo/JsongenTest.java
@@ -1,16 +1,71 @@
 package test.java.aydoo;
 
 import main.java.aydoo.jsongen;
-
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class JsongenTest {
 
 	@Test
-	public void registroJsonConCambiosPostCreacionGetterYSetterTest() {
-		String	 archivoEntrada="testFiles/definicion-producto.json";
-		String	 archivoSalida="testFiles/producto.json";
-		       jsongen.main(new String[] {archivoEntrada, archivoSalida});
+	public void ejecucionDelMainConMensajeSatisfactorioModoConsola() {
+		String archivoEntrada = "testFiles/definicion-producto.json";
+		String archivoSalida = "testFiles/producto.json";
+		String mensajeEscrituraSatisfactoria = "El archivo se escribio Correctamente";
+
+		ByteArrayOutputStream testOut = new ByteArrayOutputStream();
+		PrintStream testOutPrintStream = new PrintStream(testOut);
+		System.setOut(testOutPrintStream);
+
+		jsongen.main(new String[] { archivoEntrada, archivoSalida });
+
+		String newLine = System.getProperty("line.separator");
+		if (newLine == null)
+			newLine = "\n";
+
+		String expected = mensajeEscrituraSatisfactoria + newLine;
+
+		Assert.assertEquals(expected, testOut.toString());
+
+	}
+
+	@Test
+	public void ejecucionDelMainConMensajeDeErrorDeExtensionesModoConsola() {
+		String archivoEntrada = "testFiles/definicion-product.json";
+		String archivoSalida = "testFiles/producto.jso";
+		String mensajeErrorExtension = "Las extensiones no son iguales";
+
+		ByteArrayOutputStream testOut = new ByteArrayOutputStream();
+		PrintStream testOutPrintStream = new PrintStream(testOut);
+		System.setOut(testOutPrintStream);
+
+		jsongen.main(new String[] { archivoEntrada, archivoSalida });
+
+		String newLine = System.getProperty("line.separator");
+		if (newLine == null)
+			newLine = "\n";
+
+		String expected = mensajeErrorExtension + newLine;
+
+		Assert.assertEquals(expected, testOut.toString());
+
+	}
+
+	@Test
+	public void ejecucionDelMainConNingunParametro() {
+
+		String mensaje = "";
+		ByteArrayOutputStream testOut = new ByteArrayOutputStream();
+		PrintStream testOutPrintStream = new PrintStream(testOut);
+		System.setOut(testOutPrintStream);
+
+		jsongen.main(new String[] { " ", " ", " " });
+
+		String expected = mensaje;
+
+		Assert.assertEquals(expected, testOut.toString());
+
 	}
 
 }
